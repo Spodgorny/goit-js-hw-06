1b783121c19f3dfac3aa65bf9712eed73d9dfb59
Spodgornyy <podgornysergey@gmail.com>
2023-01-13 18:38:12 +0200

    added file

--- js/task-01.js
+++ js/task-01.js
@@ -1,15 +1,3 @@
-// const categories = document.getElementById("categories");
-
-// const ulElem = document.createElement("ul");
-
-// for ( const categori of categories) {
-//     const liElem = document.createElement("li");
-//     liElem.textContent = categori;
-//     ulElem.appendChild(liElem);
-// }
-
-// categories.appendChild(ulElem);
-
 const arrayOfCategories = document.querySelectorAll("li.item");
 console.log(`Number of categories: ${arrayOfCategories.length}`);
 
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ulElem = document.getElementById("ingredients");
+console.log(ulElem);
+
+for ( const ingredient of ingredients) {
+    const liElem = document.createElement("li");
+    liElem.classList.add('item')
+    liElem.textContent = ingredient;
+    ulElem.appendChild(liElem);
+}
